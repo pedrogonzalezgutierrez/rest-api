17000c72bd03a97c7ce35dc801375c9d354ea1cc
pedrola <pgongut@yahoo.com>
2018-11-30 21:43:09 +0000

    Using gradle

--- src/main/java/com/gvc/customer/converter/customer/CustomerMapper.java
+++ src/main/java/com/gvc/customer/converter/customer/CustomerMapper.java
@@ -2,13 +2,13 @@ package com.gvc.customer.converter.customer;
 
 import com.gvc.customer.dto.customer.CustomerDTO;
 import com.gvc.customer.jpa.entity.customer.CustomerEntity;
-import fr.xebia.extras.selma.IgnoreFields;
 import fr.xebia.extras.selma.Mapper;
+import fr.xebia.extras.selma.Maps;
 
 @Mapper
 public interface CustomerMapper {
 
-    @IgnoreFields("customer")
+    @Maps(withIgnoreFields = "customer")
     CustomerDTO asDTO(CustomerEntity customerEntity);
 
     CustomerEntity asEntity(CustomerDTO customerDTO);
