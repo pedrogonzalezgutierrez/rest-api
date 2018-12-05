870de09760698689f2e7a41eeb6286936581bded
pedrola <pgongut@yahoo.com>
2018-12-05 00:14:24 +0000

    Delete rubbish

--- src/main/java/com/kiesoft/userapi/jpa/repository/customer/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kiesoft.userapi.jpa.repository.customer;
-
-import com.kiesoft.userapi.jpa.entity.customer.CustomerEntity;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import java.util.UUID;
-
-public interface CustomerRepository extends PagingAndSortingRepository<CustomerEntity, UUID> {
-}
