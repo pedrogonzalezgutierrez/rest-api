964e1a3dae11b0c76a1261bb8c44604d9420e125
Pedro Gonzalez Gutierrez <pedro.gutierrez@ladbrokescoral.com>
2018-12-06 18:52:49 +0000

    Add validator for AddRole

--- src/test/groovy/com/kiesoft/userapi/validator/user/AddRoleDTOValidatorSpec.groovy
+++ src/test/groovy/com/kiesoft/userapi/validator/user/AddRoleDTOValidatorSpec.groovy
@@ -1,10 +1,13 @@
 package com.kiesoft.userapi.validator.user
 
+
 import com.kiesoft.userapi.dto.role.RoleDTO
 import com.kiesoft.userapi.dto.user.AddRoleDTO
 import com.kiesoft.userapi.service.role.RoleService
 import com.kiesoft.userapi.validator.DefaultValidatorHelper
+import com.kiesoft.userapi.validator.role.RoleProperties
 import org.springframework.core.env.Environment
+import org.springframework.validation.BeanPropertyBindingResult
 import spock.lang.Specification
 
 class AddRoleDTOValidatorSpec extends Specification {
@@ -14,10 +17,99 @@ class AddRoleDTOValidatorSpec extends Specification {
     final roleService = Mock(RoleService)
     final addRoleDTOValidator = new AddRoleDTOValidator(validatorHelper, env, roleService)
 
-    final addRoleDTO = new AddRoleDTO.Builder()
+    final roleDTO = new RoleDTO.Builder()
             .id(UUID.randomUUID())
             .name("ROLE_ADMIN")
             .build()
 
+    def "validation success"() {
+        given:
+        final addRoleDTO = new AddRoleDTO.Builder()
+                .name("ROLE_ADMIN")
+                .build()
+        final errors = new BeanPropertyBindingResult(addRoleDTO, "addRoleDTO")
+
+        and:
+        env.getProperty(RoleProperties.NAME_LENGTH_MIN) >> 5
+        env.getProperty(RoleProperties.NAME_LENGTH_MAX) >> 10
+
+        and:
+        roleService.findByName(_ as String) >> Optional.of(roleDTO)
+
+        when:
+        addRoleDTOValidator.validate(addRoleDTO, errors)
+
+        then:
+        !errors.hasErrors()
+    }
+
+    def "validation fails when missing name"() {
+        given:
+        final addRoleDTO = new AddRoleDTO.Builder().build()
+        final errors = new BeanPropertyBindingResult(addRoleDTO, "addRoleDTO")
+
+        when:
+        addRoleDTOValidator.validate(addRoleDTO, errors)
+
+        then:
+        errors.getErrorCount() == 1
+    }
+
+    def "validation fails when name too small"() {
+        given:
+        final addRoleDTO = new AddRoleDTO.Builder()
+                .name("ROLE")
+                .build()
+        final errors = new BeanPropertyBindingResult(addRoleDTO, "addRoleDTO")
+
+        and:
+        env.getProperty(RoleProperties.NAME_LENGTH_MIN) >> 5
+        env.getProperty(RoleProperties.NAME_LENGTH_MAX) >> 10
+
+        when:
+        addRoleDTOValidator.validate(addRoleDTO, errors)
+
+        then:
+        errors.getErrorCount() == 1
+    }
+
+    def "validation fails when name too big"() {
+        given:
+        final addRoleDTO = new AddRoleDTO.Builder()
+                .name("ROLE_SUPER_LONG")
+                .build()
+        final errors = new BeanPropertyBindingResult(addRoleDTO, "addRoleDTO")
+
+        and:
+        env.getProperty(RoleProperties.NAME_LENGTH_MIN) >> 5
+        env.getProperty(RoleProperties.NAME_LENGTH_MAX) >> 10
+
+        when:
+        addRoleDTOValidator.validate(addRoleDTO, errors)
+
+        then:
+        errors.getErrorCount() == 1
+    }
+
+    def "validation fails when role does not exist"() {
+        given:
+        final addRoleDTO = new AddRoleDTO.Builder()
+                .name("ROLE_ADMIN")
+                .build()
+        final errors = new BeanPropertyBindingResult(addRoleDTO, "addRoleDTO")
+
+        and:
+        env.getProperty(RoleProperties.NAME_LENGTH_MIN) >> 5
+        env.getProperty(RoleProperties.NAME_LENGTH_MAX) >> 10
+
+        and:
+        roleService.findByName(_ as String) >> Optional.empty()
+
+        when:
+        addRoleDTOValidator.validate(addRoleDTO, errors)
+
+        then:
+        errors.getErrorCount() == 1
+    }
 
 }
