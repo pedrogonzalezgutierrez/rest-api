a38b755b09c4fb7fbe7ea14ae1d688b5eea817a1
Pedro Gonzalez Gutierrez <pedro.gutierrez@ladbrokescoral.com>
2018-12-13 16:53:34 +0000

    Update test

--- src/test/groovy/com/kiesoft/userapi/controller/error/ApiExceptionHandlerSpec.groovy
+++ src/test/groovy/com/kiesoft/userapi/controller/error/ApiExceptionHandlerSpec.groovy
@@ -1,6 +1,5 @@
 package com.kiesoft.userapi.controller.error
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.validation.BindingResult
@@ -12,69 +11,64 @@ import spock.lang.Specification
 import static org.assertj.core.api.Assertions.assertThat
 import static org.assertj.core.api.Assertions.tuple
 
-class ApiValidationExceptionHandlerSpec extends Specification {
+class ApiExceptionHandlerSpec extends Specification {
 
-    final apiValidationExceptionHandler = new ApiValidationExceptionHandler()
-
-    final ex = Mock(MethodArgumentNotValidException)
     final headers = Mock(HttpHeaders)
     final request = Mock(WebRequest)
     final bindingResult = Mock(BindingResult)
+    final apiExceptionHandler = new ApiExceptionHandler()
 
-    final objectMapper = new ObjectMapper();
+    final fieldError1 = new FieldError(
+            "object1",
+            "stringField",
+            "SuColega",
+            true,
+            null,
+            ["argument1", "argument2"] as String[],
+            "String message")
 
-    def "will return ApiErrorsView populated"() {
+    final fieldError2 = new FieldError(
+            "object2",
+            "integerField",
+            "25",
+            false,
+            [] as String[],
+            ["argument1", "argument2"] as String[],
+            "Integer message")
+
+    final fieldError3 = new FieldError(
+            "object3",
+            "booleanField",
+            "false",
+            true,
+            ["booleanCode"] as String[],
+            ["argument1", "argument2"] as String[],
+            "Boolean message")
+
+    final fieldError4 = new FieldError(
+            "object4",
+            "doubleField",
+            "2.2D",
+            false,
+            ["doubleCode1", "doubleCode2"] as String[],
+            ["argument1", "argument2"] as String[],
+            "Double message")
+
+    def "will return ApiErrorsView populated when @Valid fails"() {
         given:
-        final fieldError1 = new FieldError(
-                "object1",
-                "stringField",
-                "SuColega",
-                true,
-                null,
-                ["argument1", "argument2"] as String[],
-                "String message")
-
-        final fieldError2 = new FieldError(
-                "object2",
-                "integerField",
-                "25",
-                false,
-                [] as String[],
-                ["argument1", "argument2"] as String[],
-                "Integer message")
-
-        final fieldError3 = new FieldError(
-                "object3",
-                "booleanField",
-                "false",
-                true,
-                ["booleanCode"] as String[],
-                ["argument1", "argument2"] as String[],
-                "Boolean message")
-
-        final fieldError4 = new FieldError(
-                "object4",
-                "doubleField",
-                "2.2D",
-                false,
-                ["doubleCode1", "doubleCode2"] as String[],
-                ["argument1", "argument2"] as String[],
-                "Double message")
-
-        and:
-        ex.getBindingResult() >> bindingResult
+        final methodArgumentNotValidException = new MethodArgumentNotValidException(null, bindingResult)
 
         and:
         bindingResult.getFieldErrors() >> [fieldError1, fieldError2, fieldError3, fieldError4]
 
         when:
-        final actual = apiValidationExceptionHandler.handleMethodArgumentNotValid(ex, headers, HttpStatus.BAD_REQUEST, request)
+        final actual = apiExceptionHandler.handleMethodArgumentNotValid(methodArgumentNotValidException, headers, HttpStatus.BAD_REQUEST, request)
 
         then:
         Objects.nonNull(actual)
 
         and:
-        assertThat(((ApiErrorsView)actual.getBody()).fieldErrors)
+        assertThat(((ApiErrorsView) actual.getBody()).fieldErrors)
                 .extracting("field", "code", "message")
                 .contains(
                 tuple("stringField", ApiErrorMessage.UNKNOWN_CODE.getCode(), "String message"),
